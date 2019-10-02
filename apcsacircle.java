9a960c4da21cd0b1effe8922f25156da9b0f1128
Kyle Goetke <goetkek@gmail.com>
2019-10-02 13:53:03 -0400

    Initial commit

--- apcsacircle.java
+++ apcsacircle.java
@@ -9,7 +9,7 @@ public class apcsacircle {
 		System.out.printf("\nEnter a diameter: ");
 		double diameter = in.nextDouble();
 		double area = Math.PI * Math.pow((diameter / 2), 2);
-		System.out.printf("The area of a circle with a diameter of %.1f is %.1f.", diameter, area);
+		System.out.printf("\nThe area of a circle with a diameter of %.1f is %.1f.", diameter, area);
 
         in.close();
     }
