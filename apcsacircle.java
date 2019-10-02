67fa0e730be276f8665c2d01c6627ccbf254685b
Kyle Goetke <kgoetke@ucvts.tec.nj.us>
2019-10-02 13:50:52 -0400

    Initial commit

---
+++ apcsacircle.java
@@ -0,0 +1,16 @@
+
+import java.util.Scanner;
+
+public class apcsacircle {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+		System.out.printf("\nEnter a diameter: ");
+		double diameter = in.nextDouble();
+		double area = Math.PI * Math.pow((diameter / 2), 2);
+		System.out.printf("The area of a circle with a diameter of %.1f is %.1f.", diameter, area);
+
+        in.close();
+    }
+}
